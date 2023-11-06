6407262169ea9872aefd5f0a4ff7503c70c30a79
Raviragul19 <raviragul3048@gmail.com>
2023-11-06 10:37:32 +0530

    Requested Changes

--- java/shuffling array.java
+++ java/shuffling array.java
@@ -1,19 +1,25 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class ShuffleArray {
     public static void main(String[] args) {
         Integer[] array = {1, 2, 3, 4, 5, 6, 7};
 
-        List<Integer> list = new ArrayList<>(array);
-        Collections.shuffle(list);
-        Integer[] shuffledArray = list.toArray(new Integer[0]);
+        shuffleArray(array);
 
         // Print the shuffled array
         System.out.print("Shuffled array: ");
-        for (Integer element : shuffledArray) {
+        for (Integer element : array) {
             System.out.print(element + " ");
         }
     }
+
+    public static void shuffleArray(Integer[] array) {
+        int n = array.length;
+        for (int i = n - 1; i > 0; i--) {
+            int j = (int) (Math.random() * (i + 1));
+            // Swap array[i] and array[j]
+            Integer temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
 }
+
